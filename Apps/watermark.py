17411ee10017c0b21393debe7f92a739f2cd62f5
Ephvuln <alex.bogatu@yahoo.com>
2021-04-18 10:08:19 +0300

    Modified watermark alghoritm

--- Apps/watermark.py
+++ Apps/watermark.py
@@ -5,62 +5,100 @@
 
 from PIL import Image
 
+class Watermarker():
 
-def applyWM(path,wm):
-	img=Image.open(path)
-	pix=img.load()
+	PREAMBLE = 0xdeadbeef
 
-	# Index for k bit in watermark
-	k=0
+	def __init__(self,path):
+		self.i = 0
+		self.j = 0
+		self.img=Image.open(path)
+		self.pix=self.img.load()
 
-	for i in range(img.size[0]):
-		for j in range(img.size[1]):
-
-			# Repeat the watermark bits
-			if (wm >> k) == 0:
-				k=0 
-
-			if (pix[i,j][0] & 1) != ((wm >> k) & 1):
-				# Invert last bit to match watermark bit
-				pix[i,j] = (pix[i,j][0] ^ 1, pix[i,j][1], pix[i,j][2])
+	def __writeToImg(self,msg,n):
+		k=0
+		# Write preamble
+		while k < n:
+			if (self.pix[self.i,self.j][0] & 1) != ((msg >> k) & 1):
+				# Invert last bit to match preamble bit
+				self.pix[self.i,self.j] = (self.pix[self.i,self.j][0] ^ 1, self.pix[self.i,self.j][1], self.pix[self.i,self.j][2])
 			k+=1
+			self.j+=1
+			if self.j >= self.img.size[1]:
+				self.j = 0
+				self.i +=1
 
-	return img
-
-
-def checkWM(path,wm):
-	img=Image.open(path)
-	pix=img.load()
-
-	# Index for k bit in watermark
-	k=0
-
-	# Checked watermark
-	hits=0
-
-	for i in range(img.size[0]):
-		for j in range(img.size[1]):
-
-			# Repeat the watermark bits
-			if (wm >> k) == 0:
-				k=0 
-
-			wm_bit = ((wm >> k) & 1)
-			if (pix[i,j][0] & 1) == wm_bit:
-				hits +=1
-
+	def __readFromImg(self,n):
+		k=0
+		msg = 0
+		# Write preamble
+		while k < n:
+			msg += (self.pix[self.i,self.j][0] & 1) << k
 			k+=1
+			self.j+=1
+			if self.j >= self.img.size[1]:
+				self.j = 0
+				self.i +=1
+		return msg
 
-	img.close()
 
-	return hits/(img.size[0]*img.size[1])
+	def applyWM(self,wm):
+
+		wmsize = len(str(wm))
+
+		if wmsize > 2**16:
+			print("Watermark msg too big.")
+
+		# Preamble + size + actual watermark
+		if (8+2+wmsize)*8 > self.img.size[0]*self.img.size[1]:
+			print("Image size too small for watermark length.")
+
+		self.i = 0
+		self.j = 0
+
+		self.__writeToImg(Watermarker.PREAMBLE,64)
+		self.__writeToImg(wmsize,16)
+		self.__writeToImg(wm,wmsize*8)
+
+		return self.img
+
+
+	def checkWM(self,wm):
+
+		self.i = 0
+		self.j = 0
+		# Index for k bit in watermark
+		k=0
+
+		# Check preamble
+		if self.__readFromImg(64) != Watermarker.PREAMBLE:
+			print("No watermark")
+			return None
+
+		# Read size
+		wmsize = self.__readFromImg(16)
+		if (8+2+wmsize)*8 > self.img.size[0]*self.img.size[1]:
+			print("Corrupted watermark")
+			return None
+
+		# Read watermark
+		wm = self.__readFromImg(wmsize*8)
+
+		return wm
 
 
 '''
 TESTING
 '''
-def testProgram(pathIN,pathTMP,wm):
-	i = applyWM(pathIN,wm)
+if __name__ == '__main__':
+	import binascii
+	pathIN = './Sample_Images/Original/lena.png'
+	pathTMP = './Sample_Images/Test/lena.png'
+	#wm = int(binascii.hexlify(b'test'))
+	wm = 1337
+	w = Watermarker(pathIN)
+	i = w.applyWM(wm)
 	i.save(pathTMP)
-	i.close()
-	print("Checked percentage:",checkWM(pathTMP,wm))
+
+	w = Watermarker(pathTMP)
+	print("Watermark in file:",w.checkWM(wm))
